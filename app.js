4b653dfd3acb11f9a86e917be67da432c012b311
serendipity80-dev <68223867+serendipity80-dev@users.noreply.github.com>
2020-08-29 00:13:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('/home/dci/Documents/simple-website'));
+app.use(express.static('/home/dci/Documents/Tasks/Grid_task'));
 
-const port = 2993;
+const port = 2929;
 app.listen(port, function(){
     console.log(`Server is listening to the port: ${port}`);
 });
